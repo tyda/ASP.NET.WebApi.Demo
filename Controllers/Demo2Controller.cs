92a721cac4723772705ae1361c0ccd48f6459d5a
spring <tyda1206@gmail.com>
2021-01-23 23:25:24 +0800

    add demo2

---
+++ Controllers/Demo2Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ASP.NET.WebApi.Demo.Controllers
+{
+    public class Demo2Controller : Controller
+    {
+        // GET: Demo2
+        public int Index([FromBody] IEnumerable<Product> data)
+        {
+            ViewData["items"] = data;
+            return data.Count();
+        }
+    }
+}
